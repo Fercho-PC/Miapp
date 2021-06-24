34a77be7feda163f0ce5dcc233e537a4c8fa0294
Fercho-PC <fercho20pc@gmail.com>
2021-06-24 15:38:46 -0600

    Esta va a ser mi app de pruebas definitiva

---
+++ src/components/Index.jsx
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react'
+import "./css/estilos.css"
+
+function index() {
+    return (
+        
+        <Fragment>
+            <div className="cont">
+                <h1 className="h1">Hola</h1>
+            </div>
+        </Fragment>
+        
+    )
+}
+
+export default index
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/*
+import App from './App.jsx';
+*/
+//Estos son mios
+import Index from './components/Index';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/*Hago uso de fragment para separar los divs y poder darles instrucciones logicas a cada uno*/}
+    <Fragment>
+      <div>
+        {/* Mi index es la pagina principal */}
+        <Index />
+      </div>
+    </Fragment>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
